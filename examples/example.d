32bc585b197476304fee195279d52be4babc86f7
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-09-08 15:27:38 -0700

    Added protection against SDL loading failure.

--- examples/example.d
+++ examples/example.d
@@ -6,7 +6,11 @@ import message_box : showMessageBox;
 int main() {
 	import derelict.sdl2.sdl : DerelictSDL2;
 	import derelict.util.loader : SharedLibVersion;
-	DerelictSDL2.load(SharedLibVersion(2, 0, 2));
+	try {
+		DerelictSDL2.load(SharedLibVersion(2, 0, 2));
+	} catch (Throwable) {
+
+	}
 
 	showMessageBox("Example text goes here ...");
 	stdout.writefln("Dialog ...");
