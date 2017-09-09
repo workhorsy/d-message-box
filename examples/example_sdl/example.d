6225f41b9dec2c732dcd381060807b1f8e66e534
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-09-09 10:50:08 -0700

    Cleanup of the comments.

--- examples/example_sdl/example.d
+++ examples/example_sdl/example.d
@@ -1,17 +1,21 @@
 
-import std.stdio : stdout, stderr;
-import message_box : showMessageBox, IconType;
+
 
 
 int main() {
 	import derelict.sdl2.sdl : DerelictSDL2;
 	import derelict.util.loader : SharedLibVersion;
+	import message_box : showMessageBox, IconType;
+	import std.stdio : stdout, stderr;
+
+	// Load SDL
 	try {
 		DerelictSDL2.load(SharedLibVersion(2, 0, 2));
 	} catch (Throwable) {
 
 	}
 
+	// Show the message box
 	if (! showMessageBox("Birthday", "Today is Bob's Birthday!", IconType.Information)) {
 		stderr.writefln("Failed to show message box.");
 	}
