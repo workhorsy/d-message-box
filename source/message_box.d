2f6e8150b8399e4f58f04bcd9359221c26143804
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-09-23 21:54:03 -0700

    Removed old message box code.

--- source/message_box.d
+++ source/message_box.d
@@ -141,6 +141,13 @@ class MessageBox {
 	import message_box_zenity : MessageBoxZenity;
 	import message_box_kdialog : MessageBoxKdialog;
 
+	/++
+	Shows the message box with the desired title, message, and icon.
+	Params:
+	 title = The string to show in the message box title
+	 message = The string to show in the message box body
+	 icon = The type of icon to show in the message box
+	+/
 	this(string title, string message, IconType icon_type) {
 		if (MessageBoxSDL.isSupported()) {
 			_dialog = new MessageBoxSDL(title, message, icon_type);
@@ -164,114 +171,6 @@ class MessageBox {
 	}
 
 	MessageBoxBase _dialog;
-}
-
-private bool showMessageBoxSDL(string title, string message, IconType icon) {
-	version (Have_derelict_sdl2) {
-		import std.string : toStringz;
-		import derelict.sdl2.sdl : DerelictSDL2, SDL_ShowSimpleMessageBox,
-			SDL_MESSAGEBOX_INFORMATION, SDL_MESSAGEBOX_ERROR, SDL_MESSAGEBOX_WARNING;
-
-		uint flags = 0;
-		final switch (icon) {
-			case IconType.None: flags = 0; break;
-			case IconType.Information: flags = SDL_MESSAGEBOX_INFORMATION; break;
-			case IconType.Error: flags = SDL_MESSAGEBOX_ERROR; break;
-			case IconType.Warning: flags = SDL_MESSAGEBOX_WARNING; break;
-		}
-
-		// Try the SDL message box
-		if (DerelictSDL2.isLoaded()) {
-			if (SDL_ShowSimpleMessageBox(flags, title.toStringz, message.toStringz, null) == 0) {
-				return true;
-			}
-		}
-	}
-
-	return false;
-}
-
-private bool showMessageBoxWindows(string title, string message, IconType icon) {
-	version (Windows) {
-		import core.runtime;
-		import core.sys.windows.windows;
-		import std.utf : toUTFz;
-
-		int flags = 0;
-		final switch (icon) {
-			case IconType.None: flags = 0; break;
-			case IconType.Information: flags = MB_ICONINFORMATION; break;
-			case IconType.Error: flags = MB_ICONERROR; break;
-			case IconType.Warning: flags = MB_ICONWARNING; break;
-		}
-
-		int status = MessageBox(NULL, message.toUTFz!(const(wchar)*), title.toUTFz!(const(wchar)*), MB_OK | flags);
-		if (status == 0) {
-			return false;
-		}
-
-		return false;
-	} else {
-		return false;
-	}
-}
-
-private bool showMessageBoxZenity(string title, string message, IconType icon) {
-	import std.process : pipeProcess, wait, Redirect;
-	import helpers : programPaths, logProgramOutput;
-
-	string flags = "";
-	final switch (icon) {
-		case IconType.None: flags = "--info"; break;
-		case IconType.Information: flags = "--info"; break;
-		case IconType.Error: flags = "--error"; break;
-		case IconType.Warning: flags = "--warning"; break;
-	}
-
-	// Show the message using Zenity
-	string[] paths = programPaths(["zenity"]);
-	if (paths.length > 0) {
-		string[] args = [paths[0], flags, "--title=" ~ title, "--text=" ~ message];
-		auto pipes = pipeProcess(args, Redirect.stdin | Redirect.stdout | Redirect.stderr);
-		int status = wait(pipes.pid);
-		if (message_box_use_log) {
-			logProgramOutput(pipes);
-		}
-		if (status == 0) {
-			return true;
-		}
-	}
-
-	return false;
-}
-
-private bool showMessageBoxKdialog(string title, string message, IconType icon) {
-	import std.process : pipeProcess, wait, Redirect;
-	import helpers : programPaths, logProgramOutput;
-
-	string flags = "";
-	final switch (icon) {
-		case IconType.None: flags = "--msgbox"; break;
-		case IconType.Information: flags = "--msgbox"; break;
-		case IconType.Error: flags = "--error"; break;
-		case IconType.Warning: flags = "--sorry"; break;
-	}
-
-	// Show the message using kdialog
-	string[] paths = programPaths(["kdialog"]);
-	if (paths.length > 0) {
-		string[] args = [paths[0], flags, message, "--title", title];
-		auto pipes = pipeProcess(args, Redirect.stdin | Redirect.stdout | Redirect.stderr);
-		int status = wait(pipes.pid);
-		if (message_box_use_log) {
-			logProgramOutput(pipes);
-		}
-		if (status == 0) {
-			return true;
-		}
-	}
-
-	return false;
 }
 
 private bool showMessageBoxGxmessage(string title, string message, IconType icon) {
@@ -301,40 +200,4 @@ private bool showMessageBoxGxmessage(string title, string message, IconType icon
 	}
 
 	return false;
-}
-
-/++
-Shows the message box with the desired title, message, and icon.
-Params:
- title = The string to show in the message box title
- message = The string to show in the message box body
- icon = The type of icon to show in the message box
-+/
-
-bool showMessageBox(string title, string message, IconType icon) {
-	import std.stdio : stderr;
-
-	bool did_show = false;
-
-	if (! did_show) {
-		did_show = showMessageBoxSDL(title, message, icon);
-	}
-
-	if (! did_show) {
-		did_show = showMessageBoxWindows(title, message, icon);
-	}
-
-	if (! did_show) {
-		did_show = showMessageBoxZenity(title, message, icon);
-	}
-
-	if (! did_show) {
-		did_show = showMessageBoxKdialog(title, message, icon);
-	}
-
-	if (! did_show) {
-		did_show = showMessageBoxGxmessage(title, message, icon);
-	}
-
-	return did_show;
 }
