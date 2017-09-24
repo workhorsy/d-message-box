0b0fb6a4e176a7aa6dc817f388420d8898acc70a
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-09-23 21:18:09 -0700

    Added new Win32 message box.

---
+++ source/message_box_win32.d
@@ -0,0 +1,48 @@
+// Copyright (c) 2017 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+// Boost Software License - Version 1.0
+// A simple message box for the D programming language
+// https://github.com/workhorsy/d-message-box
+
+
+module message_box_win32;
+
+import message_box : MessageBoxBase, IconType;
+
+
+class MessageBoxWin32 : MessageBoxBase {
+	this(string title, string message, IconType icon_type) {
+		super(title, message, icon_type);
+	}
+
+	override void show() {
+		version (Windows) {
+			import core.runtime;
+			import core.sys.windows.windows;
+			import std.utf : toUTFz;
+
+			int flags = 0;
+			final switch (icon) {
+				case IconType.None: flags = 0; break;
+				case IconType.Information: flags = MB_ICONINFORMATION; break;
+				case IconType.Error: flags = MB_ICONERROR; break;
+				case IconType.Warning: flags = MB_ICONWARNING; break;
+			}
+
+			int status = MessageBox(NULL, message.toUTFz!(const(wchar)*), title.toUTFz!(const(wchar)*), MB_OK | flags);
+			if (status != 0) {
+				if (_on_error_cb) _on_error_cb(new Exception("Failed to show Win32 message box."));
+			}
+		} else {
+			if (_on_error_cb) _on_error_cb(new Exception("Failed to load Win32."));
+		}
+	}
+
+	static bool isSupported() {
+		version (Windows) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
