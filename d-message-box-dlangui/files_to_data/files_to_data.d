7415bb6a53fbdece23c73bcafe5872421f26c790
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-11-06 00:19:18 -0800

    Added more copyright headers to files.

--- d-message-box-dlangui/files_to_data/files_to_data.d
+++ d-message-box-dlangui/files_to_data/files_to_data.d
@@ -1,3 +1,8 @@
+// Copyright (c) 2017 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+// Boost Software License - Version 1.0
+// A simple message box for the D programming language
+// https://github.com/workhorsy/d-message-box
+
 
 import std.stdio : stdout, stderr;
 import compressed_file : CompressedFile;
--- source/compressed_file.d
+++ source/compressed_file.d
@@ -1,4 +1,7 @@
-
+// Copyright (c) 2017 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+// Boost Software License - Version 1.0
+// A simple message box for the D programming language
+// https://github.com/workhorsy/d-message-box
 
 struct CompressedFile {
 	string name;
