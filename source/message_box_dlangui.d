3bfd566fc6ff85eb0dd0e336c1eb90aae2484716
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2018-03-08 01:28:09 -0800

    Updated copyrights.

--- source/message_box_dlangui.d
+++ source/message_box_dlangui.d
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+// Copyright (c) 2017-2018 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
 // Boost Software License - Version 1.0
 // A simple message box for the D programming language
 // https://github.com/workhorsy/d-message-box
@@ -68,4 +68,3 @@ class MessageBoxDlangUI : MessageBoxBase {
 		}
 	}
 }
-
--- source/message_box_sdl.d
+++ source/message_box_sdl.d
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+// Copyright (c) 2017-2018 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
 // Boost Software License - Version 1.0
 // A simple message box for the D programming language
 // https://github.com/workhorsy/d-message-box
@@ -41,4 +41,3 @@ class MessageBoxSDL : MessageBoxBase {
 		return is_sdl2_loadable;
 	}
 }
-
